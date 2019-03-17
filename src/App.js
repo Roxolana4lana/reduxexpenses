9ce8b16afb126294a699e1a1531d003080ab8926
Roxolana <roxolanapavlyk@gmail.com>
2019-03-17 23:04:40 +0100

    added containers to the main

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import CreatePost from './containers/CreatePost'
 import './App.css';
 import PostList from './containers/PostList';
+import CreateIncome from './containers/CreateIncome';
+import IncomesList from './containers/IncomesList';
 
+ 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <div><CreatePost/></div>
-      <div><PostList/></div>
+<div><CreatePost /></div>
+ <div><PostList /></div>
+        <div><CreateIncome /></div>
+        <div><IncomesList /></div>
       </div>
     );
   }
