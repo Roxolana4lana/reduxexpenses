1a50ff23c43169700ed2c6fd2b8015f89502dd67
Roxolana <roxolanapavlyk@gmail.com>
2019-03-17 23:11:15 +0100

    added income reducer

---
+++ src/reducers/incomeReducer.js
@@ -0,0 +1,13 @@
+import { ADD_INCOME, DELETE_INCOME, FETCH_INCOME } from '../actions/types'
+
+export default function incomeReducer(state = [], action) {
+    switch (action.type) {
+        case ADD_INCOME:
+            return [...state, action.payload]
+        case DELETE_INCOME:
+            return state.filter(income => income.id !== action.payload.id)
+        case FETCH_INCOME:
+            return action.incomes
+        default: return state
+    }
+}
