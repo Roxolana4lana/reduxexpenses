d78dfdb2832a831d76e7bc002d7ac41d15e21861
Roxolana <roxolanapavlyk@gmail.com>
2019-03-17 23:06:33 +0100

    added types for income

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,7 @@
 export const ADD_POST = 'ADD_POST';
 export const DELETE_POST = 'DELETE_POST';
-
 export const FETCH_POST = 'FETCH_POST';
+
+export const ADD_INCOME = 'ADD_INCOME'
+export const DELETE_INCOME = 'DELETE_INCOME'
+export const FETCH_INCOME = 'FETCH_INCOME'
