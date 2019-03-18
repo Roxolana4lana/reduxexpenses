793de92d68b82a1bfbd00e601b55e39a072e2b0b
Roxolana <roxolanapavlyk@gmail.com>
2019-03-18 14:00:00 +0100

    added button to open portal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link, withRouter } from 'react-router-dom'
+import Portal from './Portal'
+import Info from './Info';
 
 
-const Navbar = () => {
-    return (
+class Navbar extends Component {
+    state = { showModal: false }
+    handleShowMessageClick = () => this.setState({ showModal: true })
+    handleCloseModal = () => this.setState({ showModal: false })
+    render() {
+        return (
         <div className='navigation'>
             <ul className="list_right">
+                    <li>
+                        <h2 onClick={this.handleShowMessageClick}>Details</h2> </li>
+                        
+                   
+                   
                 <li><Link to='/'>Expenses</Link></li>
                 <li><Link to='/incomes'>Incomes</Link></li>
+                    {this.state.showModal ? (
+                        <Portal onClose={this.handleCloseModal}>
+                            <Info/>
+            </Portal>
+                    ) : null}
             </ul>
+           
         </div>
     )
+}
 }
 
 export default withRouter(Navbar)
